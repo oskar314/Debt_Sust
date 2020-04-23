bdd4c66ee04df090411a3395da9605e6e3fb6cdb
oskar314 <oscar.martinez.c@outlook.com>
2020-04-23 09:36:31 -0400

    Main Script for the analysis

---
+++ Main.R
@@ -0,0 +1,5 @@
+##############################################################################
+# Main Script to perfomr the analysis of the paper about Debt Sustainability #
+##############################################################################
+
+# 1. Download the data from OECD database
