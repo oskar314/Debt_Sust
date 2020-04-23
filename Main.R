3b61f875035255008221f57c09efea6ddaca12f6
oskar314 <oscar.martinez.c@outlook.com>
2020-04-23 09:59:02 -0400

    Main R file to perform the analysis

--- Main.R
+++ Main.R
@@ -2,4 +2,11 @@
 # Main Script to perfomr the analysis of the paper about Debt Sustainability #
 ##############################################################################
 
-# 1. Download the data from OECD database
+# 1. Read the data (General Government Debt as a percentage of GDP from OECD countries)
+if (!file.exists("data")) {
+        dir.create("data")
+}
+
+debtdata <- read.csv("./data/debt.csv", colClasses = "character")
+debtdata <- data.frame(CountryCode = debtdata$ï..LOCATION, Year = debtdata$TIME,
+                       DebtRatio = debtdata$Value)
